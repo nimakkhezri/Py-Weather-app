00fc9da9a618e95bdd2160fb2c9d0f83749b3333
Nima Koohkhezri <nimakoohkhezri@gmail.com>
2024-07-15 12:58:48 +0330

    Several functions of the weather class were made private

--- Weather.py
+++ Weather.py
@@ -4,19 +4,19 @@ import requests, json
 
 class Weather:
     def __init__(self, city = "Tehran", api_key = "c72f8b108bde29d725e0fe467e42e228") -> None:
-        self.api_key = api_key
-        self.city = city
-        self.set_url()
+        self.__api_key = api_key
+        self.__city = city
+        self.__set_url()
 
-    def get_responce(self) -> json:
-        res = requests.get(self.url)
+    def __get_responce(self) -> json:
+        res = requests.get(self.__url)
         return res.json() 
 
-    def set_url(self) -> None:
-        self.url = f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}"
+    def __set_url(self) -> None:
+        self.__url = f"https://api.openweathermap.org/data/2.5/weather?q={self.__city}&appid={self.__api_key}"
 
     def get_kelvin_temp(self) -> float:
-        data = self.get_responce()
+        data = self.__get_responce()
         main = data["main"]
         return main["temp"]
         
@@ -26,12 +26,12 @@ class Weather:
         return round(celsius)
     
     def get_max_kelvin_temp(self) -> float:
-        data = self.get_responce()
+        data = self.__get_responce()
         main = data["main"]
         return main["temp_max"]
     
     def get_min_kelvin_temp(self) -> float:
-        data = self.get_responce()
+        data = self.__get_responce()
         main = data["main"]
         return main["temp_min"]
     
@@ -46,26 +46,26 @@ class Weather:
         return round(celsius)
     
     def get_description(self) -> str:
-        data = self.get_responce()
+        data = self.__get_responce()
         description = data["weather"][0]["main"] + " : " + data["weather"][0]["description"]
         return description
 
     def set_city(self, city) -> None :
         self.city = city
-        self.set_url()
+        self.__set_url()
 
     def get_wind_speed(self) -> float:
-        data = self.get_responce()
+        data = self.__get_responce()
         return data["wind"]["speed"]
     
     def get_country(self) -> str:
-        data = self.get_responce()
+        data = self.__get_responce()
         return data["sys"]["country"]
     
     def get_city(self) -> str:
-        data = self.get_responce()
+        data = self.__get_responce()
         return data["name"]
     
     def get_pressure(self) -> int:
-        data = self.get_responce()
+        data = self.__get_responce()
         return data["main"]["pressure"]

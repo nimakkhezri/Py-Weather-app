d7512a8978a6d7688759d3176896862a1e770c97
Nima Koohkhezri <nimakoohkhezri@gmail.com>
2024-07-15 12:34:36 +0330

    get request and temp added

--- Weather.py
+++ Weather.py
@@ -6,10 +6,25 @@ class Weather:
     def __init__(self, city = "Tehran", api_key = "c72f8b108bde29d725e0fe467e42e228") -> None:
         self.api_key = api_key
         self.city = city
+        self.set_url()
 
-    def get_temp(self) -> float :
+    def get_responce(self) -> json:
+        res = requests.get(self.url)
+        return res.json() 
+
+    def set_url(self) -> None:
+        self.url = f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}"
+
+    def get_kelvin_temp(self) -> float:
+        data = self.get_responce()
+        main = data["main"]
+        return main["temp"]
         
-
+    def get_celcius_temp(self) -> int:
+        kelvin = self.get_kelvin_temp()
+        celsius = kelvin - 273.15
+        return round(celsius)
 
     def set_city(self, city) -> None :
         self.city = city
+        self.set_url()
